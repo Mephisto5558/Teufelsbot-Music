37c9349abe6d7f511ef2b9aa5e803bb0264a68d6
Mephisto555 <justinderererste@gmail.com>
2022-04-30 14:36:07 +0200

    cooldown fix maybe

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log("Starting...") 
+console.log("Starting...")
 
 const { Client, Collection } = require("discord.js");
 
 const fs = require("fs");
-fs.rmSync('./Logs/debug.log', {force: true});
+fs.rmSync('./Logs/debug.log', { force: true });
 
 const client = new Client({ intents: 32767 });
 client.on('debug', debug => fs.appendFileSync('./Logs/debug.log', debug + `\n`));
@@ -17,28 +17,25 @@ client.events = new Collection();
 client.cooldown = new Map();
 client.commands = new Collection();
 client.categories = fs.readdirSync("./Commands/");
-client.slashCommandList = []; 
-client.sleep = function sleep(milliseconds) {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
-}; 
+client.slashCommandList = [];
+client.sleep = function sleep(milliseconds) { 
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+};
 
 //const client2 = client;
 //client2.prefix = '.2';
 //client2.userID = '964315306015211560'; 
 
-fs.readdirSync("./Handlers").filter((file) => file.endsWith("_handler.js")).forEach((handler) => {
-  require(`./Handlers/${handler}`)(client);
-  //require(`./Handlers/${handler}`)(client2);
-}); 
+fs.readdirSync("./Handlers").filter((file) => file.endsWith("_handler.js")).forEach((handler) => { 
+  require(`./Handlers/${handler}`)(client);  //require(`./Handlers/${handler}`)(client2);
+});
 
 
-client.login(process.env.token)
-  .then(console.log("Logged in (1)")); 
+client.login(process.env.token) .then(console.log("Logged in (1)"));
 
 //client2.login(process.env.token2)
 //  .then(console.log("Logged in (2)")); 
 
-process.on('exit', async () => {
-  client.destroy();
-  //client2.destroy();
+process.on('exit', async() => {
+  client.destroy();   //client2.destroy();
 });
