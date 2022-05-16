dc26675378fad9a471eb72f7e097db56adf76216
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-05-16 13:28:26 +0200

    added cancel button, fixed button spam posibility

--- Commands/Music/play.js
+++ Commands/Music/play.js
@@ -1,6 +1,7 @@
-const { Command } = require("reconlx");
-const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
-const player = require('../../Handlers/music_player');
+const
+  { Command } = require("reconlx"),
+  { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js'),
+  player = require('../../Handlers/music_player');
 
 module.exports = new Command({
   name: 'play',
@@ -23,11 +24,15 @@ module.exports = new Command({
     }
   ],
 
-  run: async (_, interaction) => {
-    let member = interaction.guild.members.cache.get(interaction.member.id);
-    let results = [];
-    let interaction0;
-    let query = interaction.options.getString('query');
+  run: async (client, interaction) => {
+    let
+      query = interaction.options.getString('query'),
+      member = interaction.guild.members.cache.get(interaction.member.id),
+      results = [],
+      interaction0,
+      i = 1,
+      rows = [],
+      row = new MessageActionRow();
 
     if(query.includes("youtube.com") || query.includes("youtu.be")) {
       player.play(member.voice.channel, query, {
@@ -35,44 +40,65 @@ module.exports = new Command({
         textChannel: interaction.channel,
         skip: interaction.options.getBoolean('skip') || false
       });
-      interaction0.delete();
       return;
     }
-    i=1;
+
     const search = await player.search(query, { type: "video", limit: 5 });
-    search.forEach((result) => {
+    for(result of search) {
       if(result.name.length > 150) result.name = `${result.name.substring(0,147)}...`;
 
       results.push(`${i++}. [${result.name}](${result.url}) by ${result.uploader.name}`);
-    });
-
-    do {
+    };
+    
+    if(results.join().length > 4096) do {
       results.pop();
     } while(results.join().length > 4096);
 
     let embed = new MessageEmbed()
       .setTitle('Please select a song. You have 30 seconds.')
       .setDescription(results.join('\r\n'));
-    
-    let row = new MessageActionRow();
+
     for(i=1; i <= results.length; i++) {
+      if(i == 6) {
+        rows.push(row);
+        row = new MessageActionRow()
+      }
 			row.addComponents(new MessageButton()
 				.setCustomId(i.toString())
 				.setLabel(i.toString())
 				.setStyle('PRIMARY')
       )
     }
+    rows.push(row);
+    row = new MessageActionRow()
+      .addComponents(new MessageButton()
+        .setCustomId('cancel')
+        .setLabel('Cancel')
+        .setStyle('DANGER')
+      );
+    
+    rows.push(row);
 
-		await interaction.followUp({ embeds: [embed], components: [row] })
-      .then(msg => {
-        interaction0 = msg;
-        msg.edit('Song selected!');
-      });
+		await interaction.editReply({ embeds: [embed], components: rows })
+      .then(msg => { interaction0 = msg });
 
-    const filter = i => i.member.id === interaction.member.id;
+    const filter = i => i.member.id == interaction.member.id;
     const collector = interaction.channel.createMessageComponentCollector({ filter, time: 30000 });
 
     collector.on('collect', async interaction2 => {
+      interaction2.deferUpdate()
+      collector.stop()
+      
+      for(row of rows) {
+        for(button of row.components) {
+          button.setDisabled(true);
+        }
+      }
+      
+      interaction.editReply({ embeds: [embed], components: rows });
+      
+      if(interaction2.customId == 'cancel') return;
+      
       await player.play(member.voice.channel, results[interaction2.customId - 1], {
         member: member,
         textChannel: interaction.channel,
@@ -80,7 +106,8 @@ module.exports = new Command({
       })
 	  });
     
-    collector.on('end', _ => {
+    collector.on('end', async _ => {
+      await client.sleep(15000)
       interaction0.delete();
     })
   }
