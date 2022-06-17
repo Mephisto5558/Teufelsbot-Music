1ce45070ff2bfc673ad8a89dab7ace4b8c11504e
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-17 20:57:16 +0200

    updated example

--- command_exapmle.js
+++ command_exapmle.js
@@ -3,9 +3,10 @@ const { Command } = require('reconlx');
 module.exports = new Command({
     name: '',
     description: '',
-    userPermissions: [],
-    cooldown: { global: 0, user: 0 },
+    permissions: { client: [], user: [] },
+    cooldown: { client: 0, user: 0 },
     category : '',
+    needsQueue: false,
 
     run: async ({ client, interaction, args }) => {
 
