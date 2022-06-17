998668fcc2b83dffb955714af96bc2b87278c74f
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-17 18:18:03 +0200

    queue is now working

--- Commands/Music/queue.js
+++ Commands/Music/queue.js
@@ -1,6 +1,7 @@
 const
   { Command } = require('reconlx'),
-  { MessageEmbed } = require('discord.js');
+  { MessageEmbed } = require('discord.js'),
+  { colors } = require('../../Settings/embed.json');
 
 module.exports = new Command({
   name: 'queue',
@@ -8,23 +9,23 @@ module.exports = new Command({
   permissions: { client: [], user: [] },
   cooldown: { client: 0, user: 2000 },
   category: 'Music',
+  needsQueue: true,
 
-  run: async (client, interaction) => {
-    const queue = client.musicPlayer.getQueue(interaction.guild.id);
-    const song = queue.songs[0];
+  run: async player => {
+    const song = player.queue.songs[0];
+    remainingSongTime = Number.prototype.toFormattedTime(player.queue.duration - player.queue.currentTime);
 
-    if (!queue?.songs) return interaction.editReply('There are no songs in the queue!')
-
-    const description = queue.songs.map((song, index) => {
+    const description = player.queue.songs.map((song, index) => {
       return `\`${index + 1}.\` **[${song.name}](${song.url})** \`[${song.formattedDuration}]\``;
     }).join('\n');
 
     const embed = new MessageEmbed()
       .setTitle('Queue')
       .setDescription(description.substring(0, 4096))
-      .addField('Queue Length', queue.songs.formattedDuration || '0:00', true)
-      .addField('Current Song', `[${song.name}](${song.url}) ${song.info.uploader?`by ${song.info.uploader}`:''}`, true);
+      .addField('Current Song', `[${song.name}](${song.url}) by ${song.uploader.name} \`${player.queue.formattedCurrentTime}\` / \`${remainingTime}\``)
+      .addField('Queue Length', player.queue.formattedDuration)
+      .setColor(colors.discord.BURPLE);
 
-    interaction.editReply({ embeds: [embed] });
+    editReply(player, { embeds: [embed] });
   }
 })
