e53429a1bca14660bba25d97a7008dd66162e870
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-24 10:37:55 +0200

    added autoplay (disabled)

---
+++ Commands/Music/autoplay.js
@@ -0,0 +1,17 @@
+const { Command } = require('reconlx');
+
+module.exports = new Command({
+  name: 'autoplay',
+  description: 'toggles YouTube autoplay',
+  permissions: { client: [], user: [] },
+  cooldown: { client: 0, user: 2000 },
+  category: 'Music',
+  needsQueue: true,
+  disabled: true,
+
+  run: async player => {
+    await player.queue.toggleAutoplay();
+
+    editReply(player, `Autoplay has been ${player.queue.autoplay ? 'enabled' : 'disabled'}.`, true);
+  }
+})
