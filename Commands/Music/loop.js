2b9b09e33056077a2a1dcf738363f88f884f5800
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-24 10:38:37 +0200

    optimized code

--- Commands/Music/loop.js
+++ Commands/Music/loop.js
@@ -2,7 +2,7 @@ const { Command } = require('reconlx');
 
 module.exports = new Command({
   name: 'loop',
-  aliases: 'repeat',
+  aliases: ['repeat'],
   description: 'Loop a song or the whole queue',
   permissions: { client: [], user: [] },
   cooldown: { global: 0, user: 2000 },
@@ -24,15 +24,8 @@ module.exports = new Command({
   run: async (player, interaction) => {
     const cmd = interaction.options.getSubcommand();
 
-    if (cmd == 'song') {
-      if (player.queue.repeatMode == 1) await player.queue.setRepeatMode(0);
-      else await player.queue.setRepeatMode(1);
-    }
-    else if (cmd == 'queue') {
-      if (player.queue.repeatMode == 2) await player.queue.setRepeatMode(0);
-      else await player.queue.setRepeatMode(2);
-    }
+    await player.queue.setRepeatMode(player.queue.repeatMode > 0 ? 0 : (cmd == 'song' ? 1 : 2));
 
-    editReply(player, `${cmd == 'song' ? 'Song' : 'Queue'} loop ${player.queue.repeatMode == 0 ? 'disabled' : 'enabled'}`,  true );
+    editReply(player, `${player.queue.repeatMode > 0 ? 'Disabled' : (cmd == 'song' ? 'Enabled song' : 'Enabled queue')} loop.`, true);
   }
 })
