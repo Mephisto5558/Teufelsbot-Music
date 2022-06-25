0f5972489b1942ab2b04366c9ceaafd3a0b24bb6
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-25 12:05:33 +0200

    imited output

--- Commands/Music/queue.js
+++ Commands/Music/queue.js
@@ -15,15 +15,18 @@ module.exports = new Command({
     const song = player.queue.songs[0];
     remainingSongTime = Number.prototype.toFormattedTime(player.queue.duration - player.queue.currentTime);
 
-    const description = player.queue.songs.map((song, index) => {
-      return `**${index + 1}** [${song.name}](${song.url}) \`[${song.formattedDuration}]\``;
+    let description = player.queue.songs.map((song, index) => {
+      if (!index) return;
+      return `**${index}**. [${song.name.length <= 75 ? song.name : song.name.substring(0, 75) + '...'}](${song.url}) \`[${song.formattedDuration}]\``;
     }).join('\n');
+
+    if (description.length > 4096) description = description.substring(0, description.substring(0, 4096).lastIndexOf('\n**')) + '\n...';
 
     const embed = new MessageEmbed()
       .setTitle('Queue')
-      .setDescription(description.substring(0, 4096))
-      .addField('Current Song', `[${song.name}](${song.url}) by ${song.uploader.name} \`${player.queue.formattedCurrentTime}\` / \`${remainingSongTime}\``)
-      .addField('Queue Length', player.queue.formattedDuration, true)
+      .setDescription(description)
+      .addField('Current Song', `[${song.name.length <= 75 ? song.name : song.name.substring(0, 75) + '...'}](${song.url}) by ${song.uploader.name} \`${player.queue.formattedCurrentTime}\` / \`${remainingSongTime}\``)
+      .addField('Queue Length', `${player.queue.songs.length} songs, ${player.queue.formattedDuration}`, true)
       .addField('Autoplay', player.queue.autoplay ? 'On' : 'Off', true)
       .setColor(colors.discord.BURPLE);
 
