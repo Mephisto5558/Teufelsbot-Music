4b2ab0f4db1d565cb1db6a5b3310720257990e6c
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-26 16:08:37 +0200

    formatting

--- Commands/Music/queue.js
+++ Commands/Music/queue.js
@@ -20,14 +20,14 @@ module.exports = new Command({
       return `**${index}**. [${song.name.length <= 75 ? song.name : song.name.substring(0, 75) + '...'}](${song.url}) \`[${song.formattedDuration}]\``;
     }).join('\n');
 
-    if (description.length > 4096) description = description.substring(0, description.substring(0, 4096).lastIndexOf('\n**')) + '\n...';
+    if (description.length > 4096) description = description.substring(0, description.substring(0, 4096).lastIndexOf('\n**')) + '\n**...**';
 
     const embed = new MessageEmbed()
       .setTitle('Queue')
       .setDescription(description)
       .addField('Current Song', `[${song.name.length <= 75 ? song.name : song.name.substring(0, 75) + '...'}](${song.url}) by ${song.uploader.name} \`${player.queue.formattedCurrentTime}\` / \`${remainingSongTime}\``)
-      .addField('Queue Length', `${player.queue.songs.length} songs, ${player.queue.formattedDuration}`, true)
-      .addField('Autoplay', player.queue.autoplay ? 'On' : 'Off', true)
+      .addField('Queue Length', `\`${player.queue.songs.length}\` songs, \`${player.queue.formattedDuration}\``, true)
+      .addField('Autoplay', player.queue.autoplay ? '`On`' : '`Off`', true)
       .setColor(colors.discord.BURPLE);
 
     await editReply(player, { embeds: [embed] });
