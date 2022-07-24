5938c5b3e12dd51e47152ca52e283c8fe788a8bb
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-24 21:47:25 +0200

    fixed number function

--- index.js
+++ index.js
@@ -8,50 +8,46 @@ const
 
 global.getDirectoriesSync = path => readdirSync(path, { withFileTypes: true }).filter(e => e.isDirectory()).map(directory => directory.name);
 
-Number.prototype.toFormattedTime = _ => {
+Number.prototype.toFormattedTime = function toFormattedTime() {
   if (isNaN(parseInt(this))) throw new SyntaxError(`${this} is not a valid number!`);
   if (this >= 86400) throw new RangeError(`Number cannot be bigger then 86400, got ${this}!`);
 
   return new Date(this * 1000).toISOString().substring(this < 3600 ? 14 : 11, 19);
 }
 
-Object.defineProperty(Number.prototype, 'toFormattedTime', {});
+const client = new Client({
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  allowedMentions: { parse: ['users', 'roles'] },
+  shards: 'auto',
+  retryLimit: 2,
+  intents: [
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
 
-(async _ => {
-  const client = new Client({
-    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
-    allowedMentions: { parse: ['users', 'roles'] },
-    shards: 'auto',
-    retryLimit: 2,
-    intents: [
-      GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildVoiceStates
-    ]
-  });
+client.startTime = Date.now();
+client.categories = getDirectoriesSync('./Commands');
+client.functions = {};
+client.dashboardOptionCount = {};
+client.lastRateLimit = new Collection();
+client.events = new Collection();
+client.cooldowns = new Collection();
+client.commands = new Collection();
+client.guildData = new Collection();
+client.log = (...data) => {
+  const date = new Date().toLocaleTimeString('en', { timeStyle: 'medium', hour12: false });
+  console.log(`[${date}] ${data}`)
+};
 
-  client.startTime = Date.now();
-  client.categories = getDirectoriesSync('./Commands');
-  client.functions = {};
-  client.dashboardOptionCount = {};
-  client.lastRateLimit = new Collection();
-  client.events = new Collection();
-  client.cooldowns = new Collection();
-  client.commands = new Collection();
-  client.guildData = new Collection();
-  client.log = (...data) => {
-    const date = new Date().toLocaleTimeString('en', { timeStyle: 'medium', hour12: false });
-    console.log(`[${date}] ${data}`)
-  };
+webServer.all('*', (_, res) => res.sendStatus(200));
+webServer.listen(8000, _ => client.log('Website is online'));
 
-  webServer.all('*', (_, res) => res.sendStatus(200));
-  webServer.listen(8000, _ => client.log('Website is online'));
+for (const handler of readdirSync('./Handlers')) require(`./Handlers/${handler}`)(client);
 
-  for (const handler of readdirSync('./Handlers')) require(`./Handlers/${handler}`)(client);
+client.login(process.env.token)
+  .then(_ => client.log(`Logged in`));
 
-  await client.login(process.env.token);
-  client.log(`Logged into ${client.botType}`);
-
-  process.on('exit', _ => client.destroy());
-})();
+process.on('exit', _ => client.destroy());
