64e049c703f3cec5a654a693d9ea6d2cf146de43
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-12-13 16:57:16 +0100

    fixed command

--- Commands/Music/loop.js
+++ Commands/Music/loop.js
@@ -12,10 +12,10 @@ module.exports = {
   run: async function (lang) {
     if (this.options.getSubcommand() == 'song') {
       await this.musicPlayer.setRepeatMode(this.musicPlayer.repeatMode == 0 ? 1 : 0);
-      this.sendEmbed(lang(player.queue.repeatMode == 0 ? 'song.disabled' : 'song.enabled'), { asEmbed: true });
+      this.sendEmbed(lang(this.musicPlayer.repeatMode == 0 ? 'song.disabled' : 'song.enabled'), { asEmbed: true });
     }
 
     await this.musicPlayer.setRepeatMode(this.musicPlayer.repeatMode == 0 ? 2 : 0);
-    this.sendEmbed(lang(player.queue.repeatMode == 0 ? 'queue.disabled' : 'queue.enabled'), { asEmbed: true });
+    this.sendEmbed(lang(this.musicPlayer.repeatMode == 0 ? 'queue.disabled' : 'queue.enabled'), { asEmbed: true });
   }
 };
